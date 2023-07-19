beb496ac78164405d4d9bf0f5678143ab441b665
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-07-19 16:39:33 +0200

    feat: clear form add functional clear form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Container } from "components/Container";
 import { Header } from "components/Header";
+import { Search } from "components/Search";
 
 
 function App() {
   return (
   <Container>
     <Header/>
+    {/* передаю порожні данні аби побачити як відображається елемент на сторінці */}
+    <Search hasError onSubmit={()=>{}}/>
   </Container>
   );
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,21 +3,34 @@ import styles from './Search.module.scss';
 import { ReactComponent as SearchIcon} from 'assets/icon-search.svg'
 import { Button } from 'components/Button';
 
-interface SearchProps { 
+interface SearchProps {
   onSubmit: (text:string)=> void,
   hasError: boolean,
 }
 
 export const Search = ({ hasError, onSubmit }: SearchProps) => {
-  const searchRef = useRef<HTMLFormElement | null>(null);
-  const handleSubmit = (event:React.FormEvent) => {}
+  const searchRef = useRef<HTMLInputElement | null>(null);
+  const handleSubmit = (event:React.FormEvent) => {
+    event.preventDefault();
+
+    const text = searchRef.current ? searchRef.current.value : '';
+
+    if (text) {
+      onSubmit(text);
+      // перевіряю форму на наявність і очищую
+      if(searchRef.current)
+        searchRef.current.value = ''
+    }
+
+  }
   return (
-    <form onSubmit={handleSubmit} ref={searchRef}>
+    <form onSubmit={handleSubmit} autoComplete='off'>
       <div className={styles.search}>
         <label htmlFor='search' className={styles.label}>
           <SearchIcon/>
         </label>
         <input
+          ref={searchRef}
           type='text'
           className={styles.textField}
           id='search'
@@ -30,7 +43,7 @@ export const Search = ({ hasError, onSubmit }: SearchProps) => {
           </div>
         )}
         <Button>Search</Button>
-      </div> 
+      </div>
   </form>
   )
 
