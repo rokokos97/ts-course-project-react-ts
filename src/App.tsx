5bd34e2c3cfa8307ecd7ebde507062bf320c15aa
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-07-19 14:55:49 +0200

    create Header: create component Header add styles for Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Container } from "components/Container";
+import { Header } from "components/Header";
 
 
 function App() {
-  return (<></>
+  return (
+  <Container>
+    <Header/>
+  </Container>
   );
 }
 
